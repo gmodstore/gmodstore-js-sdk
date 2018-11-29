9f26510ece79b841c980e7b974566736633d5f8f
Alex <threebowpr@gmail.com>
2018-11-29 13:49:15 -0800

    Get addons test

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+//Import the gmodstore API key from .env
+require("dotenv").config();
+
+//Require the API client
 let GmodstoreAPI = require(".");
 
-let client = new GmodstoreAPI();
+let client = new GmodstoreAPI(process.env.GMODSTORE_API_KEY);
+client.getAddons().then(console.log).catch(console.error);
